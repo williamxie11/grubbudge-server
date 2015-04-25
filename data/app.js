782a490d55507170a4b3c8d83bdeb7f72de4984d
Sukeerthi Khadri <Sukeer@users.noreply.github.com>
2015-04-25 15:50:53 -0500

    Update app.js
    Changed to port 4000.

--- data/app.js
+++ data/app.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
 console.log("Express server running on " + port);
 app.listen(process.env.PORT || port);
 
