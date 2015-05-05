fef932c8d7946782bf5243e8b085eedf9864eaa4
William Xie <wxie6@illinois.edu>
2015-05-05 02:10:20 -0500

    Added async web scraping functionality

--- data/price.js
+++ data/price.js
@@ -1,12 +1,16 @@
 /* Gets price level data by scraping each Yelp page corresponding to the restaurant data point
  * NOTE: Requires a correctly formatted output.json from restaurant.js. Reference the comment at the top of
  * restaurant.js for instructions on how to manually fix the JSON format before continuing with this script.
+ * The output must also be fixed in the same way as the restaurant.js output.
  */
 
 // Node Dependencies
-var express = require('express');
+var express = require("express");
 var app = express();
-var fs = require('fs');
+var fs = require("fs");
+var async = require("async");
+var cheerio = require("cheerio");
+var request = require("request");
 
 // Semantic replacement for console.log()
 function print(me) {
@@ -50,13 +54,46 @@ function appendJSONToFile(data, fileName) {
 
 /* ----- Category Data Retrieval Functions ----- */
 
-function scrapeRating() {
-
+function scrapeRating(data, link) {
+	//print("Request for: " + link);
+	request(link, function(error, response, html) {
+		if (error) throw error;
+		$ = cheerio.load(html);
+		var priceLevel = $(".business-attribute").html();
+		if (priceLevel == "$") {
+			data["price"] = 1;
+		}
+		else if (priceLevel == "$$") {
+			data["price"] = 2;
+		}
+		else if (priceLevel == "$$$") {
+			data["price"] = 3;
+		}
+		else if (priceLevel == "$$$$") {
+			data["price"] = 4;
+		}
+		else {
+			data["price"] = 0;
+		}
+		appendJSONToFile(data, "output_full.json");
+	});
 }
 
 function getRatings() {
 	clearFile("output_full.json");
+	var requests = [];
 	var data = getJSONFromFile("output.json");
+
+	print("#   Enumerating URLs.");
+	// Get URLs to scrape from
+	for (var i = 0; i < data.length; i++) {
+		var pt = data[i];
+		var url = pt.url;
+		var scrapeFunc = scrapeRating(pt, url);
+		requests.push(scrapeFunc);
+	}
+	print("#   Scraping Yelp pages for price data.");
+	async.seq(requests);
 }
 
 function main() {
