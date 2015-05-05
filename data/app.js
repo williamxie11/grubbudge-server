3cdf20dca44af9c2a85f58bdc0a9bbf75fdcdb00
William Xie <wxie6@illinois.edu>
2015-05-04 19:09:20 -0500

    Added retrieval and checks for mealType attribute

--- data/app.js
+++ data/app.js
@@ -38,23 +38,27 @@ function clearFile(fileName) {
 }
 
 function writeStrToFile(str, fileName) {
-
+	fs.writeFile(fileName, str, function(err) {
+		if (err) throw err;
+	})
 }
 
 function writeJSONToFile(data, fileName) {
-	var s = JSON.stringify(data, null, 3);]
+	var s = JSON.stringify(data, null, 3);
 	fs.writeFile(fileName, s, function(err) {
 		if (err) throw err;
 	});
 }
 
 function appendStrToFile(str, fileName) {
-
+	fs.appendFile(fileName, str, function(err) {
+		if (err) throw err;
+	})
 }
 
-function appendJSONToFile(data, name) {
+function appendJSONToFile(data, fileName) {
 	var s = JSON.stringify(data, null, 3);
-	var fileName = name + ".json";
+	s += ",";
 	fs.appendFile(fileName, s, function(err) {
 		if (err) throw err;
 	});
@@ -62,14 +66,49 @@ function appendJSONToFile(data, name) {
 
 /* ----- Data Retrieval Functions ----- */
 
+// An opening time is considered breakfast if it is between 5 AM (0500) and (9:59 AM) (0959) 
+function isBreakfast(open, close) {
+	if ( (open >= "0500") && (open <= "0959") ) {
+		return true;
+	}
+	return false;
+}
+
+// An opening time is considered lunch if it is between 10 AM (1000) and (3:59 PM) (1559) 
+function isLunch(open, close) {
+	if ( (open >= "0500") && ((close > "1200") || (close < "0500")) ) { 
+		return true;
+	}
+	return false;
+}
+
+// An opening time is considered dinner if it is between 4 PM (1600) and (9:59 PM) (2159) 
+function isDinner(open, close) {
+	if ( (open >= "0500") && ((close > "1600") || (close < "0500")) ) {
+		return true;
+	} 	
+	return false;
+}
+
+// An opening time is considered late night if it is between 10 PM (2200) and (4:59 AM) (0459) 
+function isLateNight(open, close) {
+	if ( (close > "2200") || (close <= "0459") ) {
+		return true;
+	}
+	return false;
+}
+
 // Callback function that appends data from Google Places API
 function addGoogleData(restaurant, lat, lon) {
 	restaurant["open"] = [];
 	restaurant["close"] = [];
+	restaurant["mealType"] = [];
+
 	var parameters = {
 		location: [lat, lon],
 		types: "restaurant"
 	};
+
 	googlePlaces.placeSearch(parameters, function (error, response) {
 		if (error) throw error;
 		var results = response["results"];
@@ -80,7 +119,9 @@ function addGoogleData(restaurant, lat, lon) {
 					var times = response.result["opening_hours"]["periods"];
 					var opens = [];
 					var closes = [];
-					for (var i = 0; i < times.length; i++) {
+
+					// Add opening and closing times for each day of the week
+					for (var i = 0; i < 7; i++) {
 						var openingTime = times[i].open["time"];
 						var closingTime = null;
 						if (times[i].close) {
@@ -93,8 +134,25 @@ function addGoogleData(restaurant, lat, lon) {
 					}
 					restaurant["open"] = opens;
 					restaurant["close"] = closes;
+
+					// Checks to set the correct mealType for each data point
+					// We are just using the first available opening hour for the time being
+					var time_open = opens[0];
+					var time_close = closes[0];
+					if (isBreakfast(time_open, time_close)) {
+						restaurant["mealType"].push("breakfast");
+					}
+					if (isLunch(time_open, time_close)) {
+						restaurant["mealType"].push("lunch");
+					}
+					if (isDinner(time_open, time_close)) {
+						restaurant["mealType"].push("dinner");
+					}
+					if (isLateNight(time_open, time_close)) {
+						restaurant["mealType"].push("latenight");
+					}
 				}
-				appendJSONToFile(restaurant, "output");
+				appendJSONToFile(restaurant, "output.json");
 			});
 		}
 	});
@@ -124,6 +182,7 @@ function addData(data) {
 				restaurant["categories"].push(rt.categories[k][0].toLowerCase());
 			}
 		}
+		restaurant["price"] = 1; // this will be set manually because price data is not given
 
 		// Set up callback requests to get more detail from Google Places
 		var latitude = rt.location.coordinate.latitude;
@@ -131,14 +190,12 @@ function addData(data) {
 
 		requests.push(addGoogleData(restaurant, latitude, longitude));
 	}
-
 	async.seq(requests);
 }
 
 // Begins sequential requests to get data starting from the Yelp API
 function getData() {
 	clearFile("output.json");
-	appendStrToFile("{", "output.json");
 	yelp.search({term: term, location: location, sort: sort}, function(error, data) {
 		var err = error;
 		var total = data["total"];
